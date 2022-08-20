59015b8159d0a241019bea0c06fcd85f7ed4e24a
Antonio V S Pontes <developer.antonio.pontes@gmail.com>
2022-08-19 22:53:29 -0300

    chore: using eslint

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MainPage from '../views/MainPage.vue'
 import PokemonDex from '../views/PokemonDex.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'MainPage',
+    component: MainPage
   },
   {
     path: '/list',
